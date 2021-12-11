a8d78ef3bf1ff5a0d7634a763775dc84f982febe
Jesse Shawl <jesse@jesse.sh>
2021-12-11 14:41:17 -0600

    initial commit

---
+++ spec/features/hello_spec.rb
@@ -0,0 +1,11 @@
+
+require 'rails_helper'
+
+RSpec.describe 'index page', type: :feature do
+  describe 'root path' do
+    it 'shows something' do
+      visit root_path
+      expect(page).to have_content('application')
+    end
+  end
+end

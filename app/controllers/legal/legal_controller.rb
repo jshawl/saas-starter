48644d4a9b90abb6464a4e679a363b717daa2a19
Jesse Shawl <jesse@jesse.sh>
2021-12-12 07:40:47 -0600

    add legal docs

---
+++ app/controllers/legal/legal_controller.rb
@@ -0,0 +1,6 @@
+module Legal
+  class LegalController < ApplicationController
+    def terms
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'application#index'
   resource :account
+  namespace :legal do
+    get 'terms', to: 'legal#terms'
+    get 'privacy', to: 'legal#privacy'
+  end
 end

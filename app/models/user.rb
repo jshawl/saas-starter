cef95f8e2ea7c4173ead7ed14048abe1f0147f7d
Jesse Shawl <jesse@jesse.sh>
2022-08-27 11:57:23 -0500

    remove stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  def increment_usage
-    subscription = Stripe::Subscription.retrieve(stripe_subscription_id)
-    Stripe::SubscriptionItem.create_usage_record(
-      subscription.items.first.id,
-      {quantity: 1, timestamp: Time.now.to_i},
-    )
-  end
 end

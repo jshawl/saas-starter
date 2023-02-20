316cfec7bb558579d88fd0fd9b132cfe40d093f4
Jesse Shawl <jesse@jesse.sh>
2023-02-19 19:23:28 -0600

    rubocop

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,69 +1,85 @@
+# frozen_string_literal: true
+
+# Payments
 class PaymentsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create, :capture]
+  skip_before_action :verify_authenticity_token, only: %i[create capture]
   before_action :authenticate_user!
 
   def create
     render json: create_order
   end
+
   def capture
     response = paypal_order_capture(params[:payment_id])
-    current_user.payments.create!(paypal_id: JSON.parse(response)["id"], details: response)
+    current_user.payments.create!(paypal_id: JSON.parse(response)['id'], details: response)
     render json: response
   end
+
   def index
     @payments = current_user.payments
   end
+
   def show
     @payment = current_user.payments.find_by_paypal_id(params[:id])
   end
+
   private
 
   def base_url
-    "https://api-m.sandbox.paypal.com"
+    'https://api-m.sandbox.paypal.com'
   end
 
-  def get_access_token
-    client_id = Rails.application.credentials.dig(:paypal, :client_id)
-    secret = Rails.application.credentials.dig(:paypal, :secret)
-    auth = Base64.strict_encode64(client_id + ":" + secret)
+  def access_token
     url = "#{base_url}/v1/oauth2/token"
     response = HTTParty.post(url, {
-      headers: {
-        "Authorization": "Basic #{auth}"
-      },
-      body: "grant_type=client_credentials",
-    })
-    response["access_token"]
+                               headers: {
+                                 Authorization: "Basic #{basic_auth}"
+                               },
+                               body: 'grant_type=client_credentials'
+                             })
+    response['access_token']
   end
+
+  def basic_auth
+    client_id = Rails.application.credentials.dig(:paypal, :client_id)
+    secret = Rails.application.credentials.dig(:paypal, :secret)
+    Base64.strict_encode64("#{client_id}:#{secret}")
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def payload
+    {
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: '0.01'
+          }
+        }
+      ]
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
 
   def create_order
     url = "#{base_url}/v2/checkout/orders"
-    response = HTTParty.post(url, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer #{get_access_token}",
-      },
-      body: ({
-        intent: "CAPTURE",
-        purchase_units: [
-          {
-            amount: {
-              currency_code: "USD",
-              value: "0.01",
-            },
-          },
-        ],
-      }).to_json
-    }).body
+    HTTParty.post(url, {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      Authorization: "Bearer #{access_token}"
+                    },
+                    body: payload.to_json
+                  }).body
   end
 
   def paypal_order_capture(id)
     url = "#{base_url}/v2/checkout/orders/#{id}/capture"
-    response = HTTParty.post(url, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer #{get_access_token}",
-      }
-    }).body
+    HTTParty.post(url, {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      Authorization: "Bearer #{access_token}"
+                    }
+                  }).body
   end
 end

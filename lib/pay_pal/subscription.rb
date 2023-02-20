bed4b1b8da1ebf265a08b7b17dfa961662a7d03d
Jesse Shawl <jesse@jesse.sh>
2023-02-20 11:17:10 -0600

    rubocop

--- lib/pay_pal/subscription.rb
+++ lib/pay_pal/subscription.rb
@@ -11,19 +11,5 @@ module PayPal
                                              { plan_id: }.to_json
                                            }).body
     end
-
-    def self.capture!(id)
-      url = "#{BASE_URL}/v2/checkout/orders/#{id}/capture"
-      PayPal.handle_response HTTParty.post(url, headers: PayPal::Authorization.headers).body
-    end
-
-    # def self.payload(amount)
-    #   {
-    #     intent: 'CAPTURE',
-    #     purchase_units: [
-    #       amount
-    #     ]
-    #   }
-    # end
   end
 end
--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe 'Subscriptions', type: :request do
       .to_return(status: 200, body: File.read('spec/mocks/paypal-oauth-post-200.json'))
     stub_request(:post, 'https://api-m.sandbox.paypal.com/v1/billing/subscriptions')
       .to_return(status: 200, body: File.read('spec/mocks/paypal-subscriptions-post-201.json'))
-    expect{  
-      post '/subscriptions', params: {plan_id: 'ABC'}
-    }.to change { users(:alice).payments.count }.by (1)
+    expect do
+      post '/subscriptions', params: { plan_id: 'ABC' }
+    end.to change { users(:alice).payments.count }.by(1)
     expect(users(:alice).payments.last.paypal_subscription_id).to eq('I-BW452GLLEP1G')
     expect(JSON.parse(response.body)['id']).to eq('I-BW452GLLEP1G')
   end

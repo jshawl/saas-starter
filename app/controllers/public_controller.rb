9b9dc17a96f9482eef2b2815cfe9a3791cd07bf9
Jesse Shawl <jesse@jesse.sh>
2023-02-25 20:32:51 -0600

    add pricing page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,5 +2,12 @@
 
 # routes that don't require authentication
 class PublicController < ApplicationController
-  def pricing; end
+  def pricing
+    @plans = Rails.cache.fetch('plans', expires_in: 1.hour) do
+      list = PayPal::Plan.list
+      list.plans.map do |plan|
+        PayPal::Plan.find(plan['id']).to_h
+      end
+    end
+  end
 end

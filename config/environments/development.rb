fe77b0a0e6d419d527ee25ae39ac604621342bf8
Jesse Shawl <jesse@jesse.sh>
2023-02-26 18:08:19 -0600

    can reset password locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  routes.default_url_options[:host] = "localhost:3000"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -39,8 +41,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.

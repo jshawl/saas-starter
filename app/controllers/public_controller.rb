bd639d97a84eaaa577a19522f1dd033fdf17e1aa
Jesse Shawl <jesse@jesse.sh>
2023-02-28 20:19:03 -0600

    rubocop

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,12 +3,12 @@
 # routes that don't require authentication
 class PublicController < ApplicationController
   def pricing
-    @plans = 
-    Rails.cache.fetch('plans', expires_in: 1.hour) do
-      list = PayPal::Plan.list
-      list.plans.map do |plan|
-        PayPal::Plan.find(plan['id']).to_h
+    @plans =
+      Rails.cache.fetch('plans', expires_in: 1.hour) do
+        list = PayPal::Plan.list
+        list.plans.map do |plan|
+          PayPal::Plan.find(plan['id']).to_h
+        end
       end
-    end
   end
 end

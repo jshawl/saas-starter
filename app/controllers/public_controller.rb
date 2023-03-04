db3f211bc5b34eab7c3fd52b2a0f30cc9c172487
Jesse Shawl <jesse@jesse.sh>
2023-03-03 21:28:46 -0600

    mobile experience improvements

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,6 +3,8 @@
 # routes that don't require authentication
 class PublicController < ApplicationController
   include Pricing
+  def index
+  end
   def pricing
     @plans = build_plans_list
   end

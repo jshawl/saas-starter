aa586cd1ccf32189f998aa32f30769db1cdc2e7e
Jesse Shawl <jesse@jesse.sh>
2023-03-04 22:33:27 -0600

    add todo

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,10 +2,23 @@
 
 # routes that don't require authentication
 class PublicController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[webhooks]
+
   include Pricing
   def index; end
 
   def pricing
     @plans = build_plans_list
+  end
+
+  def webhooks
+    if params["event_type"] == "BILLING.SUBSCRIPTION.CANCELLED"
+      paypal_subscription_id = params["resource"]["id"]
+      payment = Payment.find_by_paypal_subscription_id(params["resource"]["id"])
+      # todo verify signature
+      # https://developer.paypal.com/docs/api/webhooks/v1/#verify-webhook-signature_post
+      payment.update(details: params["resource"])
+    end
+    head 200
   end
 end

82cd89a04aa2feaf7bf3f480da6956a55ec6d8b4
Jesse Shawl <jesse@jesse.sh>
2023-03-06 16:42:01 -0600

    enable tracing

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry_dsn)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.traces_sample_rate = 1.0
 end
